40653c228dbc683ed3a3f54ac2f28b483a0a689b
janismozgis-rcs <janismozgis+rcs@gmail.com>
2022-02-22 21:01:25 +0200

    Lesson

---
+++ src/API/getTasks.js
@@ -0,0 +1,18 @@
+function getTasks() {
+    return [
+        {
+            title: 'Buy milk',
+            isCompleted: true,
+        },
+        {
+            title: 'Learn Javascript',
+            isCompleted: false,
+        },
+        {
+            title: 'Be awesome',
+            isCompleted: true,
+        },
+    ]
+}
+
+export default getTasks
--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import getTasks from "./API/getTasks";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tasks, setTasks] = useState(getTasks())
+    const [newTaskName, setNewTaskName] = useState('')
+
+    const changeTaskState = (taskIndex) => {
+        tasks[taskIndex].isCompleted = !tasks[taskIndex].isCompleted
+        setTasks([...tasks])
+    }
+
+    const deleteTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1)
+        setTasks([...tasks])
+    }
+
+    const addNewTask = () => {
+        const newTask = {
+            title: newTaskName,
+            isCompleted: false,
+        }
+        setTasks([...tasks, newTask])
+        setNewTaskName('')
+    }
+
+
+    const tasksList = tasks.map((task, index) => {
+        const textDecoration = task.isCompleted ? 'line-through' : 'none'
+        let deleteBtn = ''
+        if (task.isCompleted) {
+            deleteBtn = <button onClick={() => deleteTask(index)}>Delete</button>
+        }
+
+        return (
+            <li key={index}>
+                <input type="checkbox" checked={task.isCompleted} onChange={() => changeTaskState(index)} />
+                <span style={{textDecoration: textDecoration}}>{task.title}</span>
+                {deleteBtn}
+            </li>
+        )
+    })
+
+    return (
+        <div>
+            <h1>Todos</h1>
+            <ul>
+                {tasksList}
+            </ul>
+            <h2>Create new task</h2>
+            <input 
+                type="text" 
+                value={newTaskName} 
+                onChange={(event) => setNewTaskName(event.target.value)}
+            />
+            <button onClick={addNewTask}>Add task</button>
+        </div>
+    )
 }
 
 export default App;
